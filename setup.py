1ef3405ec0b0c506464bfd0f67656b65dba7f0c8
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-08-03 16:03:20 -0400

    Fix adding readme to the long_description in setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+    readme = readme_file.read()
 
 with open(path.join(here, 'CHANGELOG.md')) as changelog_file:
     changelog = changelog_file.read()
